64661f94fe8a0b0d1605a55480c4cf0768f7f192
Mateus Zuffo <mateushmdz@gmail.com>
2023-05-11 20:52:50 -0300

    added layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Router />
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
